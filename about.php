f2b4d24ae042e4ddf17a65a1ba244ad04a4f82ef
Girum <girumsenay84@gmail.com>
2023-06-27 12:38:42 +0300

    leave out the comment in about

--- about.php
+++ about.php
@@ -6,52 +6,52 @@ include("functions/functions.php");
 include("includes/main.php");
 ?>
 
-  <main>
-    <!-- HERO -->
-    <div class="nero">
-      <div class="nero__heading">
-        <span class="nero__bold">About</span> us
-      </div>
-      <p class="nero__text">
-      </p>
+<main>
+  <!-- HERO -->
+  <div class="nero">
+    <div class="nero__heading">
+      <span class="nero__bold">About</span> us
     </div>
-  </main>
+    <p class="nero__text">
+    </p>
+  </div>
+</main>
 
-<div id="content" ><!-- content Starts -->
-<div class="container" ><!-- container Starts -->
+<div id="content">
+  <div class="container">
 
-<div class="col-md-12" ><!-- col-md-12 Starts -->
+    <div class="col-md-12">
 
-<div class="box" ><!-- box Starts -->
+      <div class="box">
 
-<?php
+        <?php
 
-$get_about_us = "select * from about_us";
+        $get_about_us = "select * from about_us";
 
-$run_about_us = mysqli_query($con,$get_about_us);
+        $run_about_us = mysqli_query($con, $get_about_us);
 
-$row_about_us = mysqli_fetch_array($run_about_us);
+        $row_about_us = mysqli_fetch_array($run_about_us);
 
-$about_heading = $row_about_us['about_heading'];
+        $about_heading = $row_about_us['about_heading'];
 
-$about_short_desc = $row_about_us['about_short_desc'];
+        $about_short_desc = $row_about_us['about_short_desc'];
 
-$about_desc = $row_about_us['about_desc'];
+        $about_desc = $row_about_us['about_desc'];
 
-?>
+        ?>
 
-<h1> <?php echo $about_heading; ?> </h1>
+        <h1> <?php echo $about_heading; ?> </h1>
 
-<p class="lead"> <?php echo $about_short_desc; ?> </p>
+        <p class="lead"> <?php echo $about_short_desc; ?> </p>
 
-<p> <?php echo $about_desc; ?> </p>
+        <p> <?php echo $about_desc; ?> </p>
 
-</div><!-- box Ends -->
+      </div>
 
-</div><!-- col-md-12 Ends -->
+    </div>
 
-</div><!-- container Ends -->
-</div><!-- content Ends -->
+  </div>
+</div>
 
 <?php
 
@@ -64,4 +64,5 @@ include("includes/footer.php");
 <script src="js/bootstrap.min.js"></script>
 
 </body>
+
 </html>

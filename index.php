9d1bcf42b507bdd40603ff4e7e9d993f0600f412
Girum <girumsenay84@gmail.com>
2023-06-27 18:43:23 +0300

    comment from index

--- index.php
+++ index.php
@@ -6,136 +6,136 @@ include("includes/header.php");
 include("functions/functions.php");
 include("includes/main.php");
 ?>
-  <!-- Cover -->
-  <main>
-    <div class="hero">
-      <a href="shop.php" class="btn1">View all products
-</a>
+
+<main>
+  <div class="hero">
+    <a href="shop.php" class="btn1">View all products
+    </a>
+  </div>
+
+  <div class="wrapper">
+    <h1>Featured Collection<h1>
+  </div>
+
+  <div id="content" class="container">
+    <div class="row">
+
+      <?php
+      getPro();
+      ?>
+
     </div>
-    <!-- Main -->
-    <div class="wrapper">
-            <h1>Featured Collection<h1>      
-      </div>
 
-    <div id="content" class="container"><!-- container Starts -->
-    <div class="row"><!-- row Starts -->
+  </div>
 
-    <?php
-    getPro();
-    ?>
+  <footer class="page-footer">
 
-    </div><!-- row Ends -->
+    <div class="footer-nav">
+      <div class="container clearfix">
 
-    </div><!-- container Ends -->
-    <!-- FOOTER -->
-    <footer class="page-footer">
+        <div class="footer-nav__col footer-nav__col--info">
+          <div class="footer-nav__heading">Information</div>
+          <ul class="footer-nav__list">
+            <li class="footer-nav__item">
+              <a href="#" class="footer-nav__link">The brand</a>
+            </li>
+            <li class="footer-nav__item">
+              <a href="#" class="footer-nav__link">Local stores</a>
+            </li>
+            <li class="footer-nav__item">
+              <a href="#" class="footer-nav__link">Customer service</a>
+            </li>
+            <li class="footer-nav__item">
+              <a href="#" class="footer-nav__link">Privacy &amp; cookies</a>
+            </li>
+            <li class="footer-nav__item">
+              <a href="#" class="footer-nav__link">Site map</a>
+            </li>
+          </ul>
+        </div>
 
-      <div class="footer-nav">
-        <div class="container clearfix">
+        <div class="footer-nav__col footer-nav__col--whybuy">
+          <div class="footer-nav__heading">Why buy from us</div>
+          <ul class="footer-nav__list">
+            <li class="footer-nav__item">
+              <a href="#" class="footer-nav__link">Shipping &amp; returns</a>
+            </li>
+            <li class="footer-nav__item">
+              <a href="#" class="footer-nav__link">Secure shipping</a>
+            </li>
+            <li class="footer-nav__item">
+              <a href="#" class="footer-nav__link">Testimonials</a>
+            </li>
+            <li class="footer-nav__item">
+              <a href="#" class="footer-nav__link">Award winning</a>
+            </li>
+            <li class="footer-nav__item">
+              <a href="#" class="footer-nav__link">Ethical trading</a>
+            </li>
+          </ul>
+        </div>
 
-          <div class="footer-nav__col footer-nav__col--info">
-            <div class="footer-nav__heading">Information</div>
-            <ul class="footer-nav__list">
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">The brand</a>
-              </li>
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">Local stores</a>
-              </li>
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">Customer service</a>
-              </li>
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">Privacy &amp; cookies</a>
-              </li>
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">Site map</a>
-              </li>
-            </ul>
-          </div>
+        <div class="footer-nav__col footer-nav__col--account">
+          <div class="footer-nav__heading">Your account</div>
+          <ul class="footer-nav__list">
+            <li class="footer-nav__item">
+              <a href="#" class="footer-nav__link">Sign in</a>
+            </li>
+            <li class="footer-nav__item">
+              <a href="#" class="footer-nav__link">Register</a>
+            </li>
+            <li class="footer-nav__item">
+              <a href="#" class="footer-nav__link">View cart</a>
+            </li>
+            <li class="footer-nav__item">
+              <a href="#" class="footer-nav__link">View your lookbook</a>
+            </li>
+            <li class="footer-nav__item">
+              <a href="#" class="footer-nav__link">Track an order</a>
+            </li>
+            <li class="footer-nav__item">
+              <a href="#" class="footer-nav__link">Update information</a>
+            </li>
+          </ul>
+        </div>
 
-          <div class="footer-nav__col footer-nav__col--whybuy">
-            <div class="footer-nav__heading">Why buy from us</div>
-            <ul class="footer-nav__list">
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">Shipping &amp; returns</a>
-              </li>
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">Secure shipping</a>
-              </li>
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">Testimonials</a>
-              </li>
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">Award winning</a>
-              </li>
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">Ethical trading</a>
-              </li>
-            </ul>
-          </div>
-
-          <div class="footer-nav__col footer-nav__col--account">
-            <div class="footer-nav__heading">Your account</div>
-            <ul class="footer-nav__list">
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">Sign in</a>
-              </li>
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">Register</a>
-              </li>
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">View cart</a>
-              </li>
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">View your lookbook</a>
-              </li>
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">Track an order</a>
-              </li>
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">Update information</a>
-              </li>
-            </ul>
-          </div>
-
-          <div class="footer-nav__col footer-nav__col--contacts">
-            <div class="footer-nav__heading">Contact details</div>
-            <address class="address">
+        <div class="footer-nav__col footer-nav__col--contacts">
+          <div class="footer-nav__heading">Contact details</div>
+          <address class="address">
             Head Office: Avenue Fashion.<br>
             180-182 Regent Street, London.
           </address>
-            <div class="phone">
-              Telephone:
-              <a class="phone__number" href="tel:0123456789">0123-456-789</a>
-            </div>
-            <div class="email">
-              Email:
-              <a href="girumDeraraAmehaBiruk@.com" class="email__addr">support@yourwebsite.com</a>
-            </div>
+          <div class="phone">
+            Telephone:
+            <a class="phone__number" href="tel:0123456789">0123-456-789</a>
           </div>
-
+          <div class="email">
+            Email:
+            <a href="girumDeraraAmehaBiruk@.com" class="email__addr">support@yourwebsite.com</a>
+          </div>
         </div>
+
       </div>
+    </div>
 
-      <div class="page-footer__subline">
-        <div class="container clearfix">
+    <div class="page-footer__subline">
+      <div class="container clearfix">
 
-          <div class="copyright">
-            &copy; <?php echo date("Y");?> Ecommerce Website-PHP&trade;
-          </div>
-
-          <div class="developer">
-            Developed by Girum  Ameha  Derara   Biruk 
-          </div>
-
-          <div class="designby">
-            Design by Girum Ameha Derara Biruk
-          </div>
-
+        <div class="copyright">
+          &copy; <?php echo date("Y"); ?> Ecommerce Website-PHP&trade;
         </div>
-      </div>
-    </footer>
-</body>
 
-</html>
+        <div class="developer">
+          Developed by Girum Ameha Derara Biruk
+        </div>
+
+        <div class="designby">
+          Design by Girum Ameha Derara Biruk
+        </div>
+
+      </div>
+    </div>
+  </footer>
+  </body>
+
+  </html>
